85bea2e8321bab4ccdfc87da8076abd71554493b
Antti Järvinen <antti.jarvinen@alupark.fi>
2018-11-07 19:46:22 +0200

    Introduce buffer to limit HTTP traffic.

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const axios = require('axios');
 
 const ruuvi = require('./lib/ruuvi');
 
+const perDeviceBufferSize = process.env.PER_DEVICE_BUFFER_SIZE || 30;
+const previousData = {};
+const buffer = [];
+
 log.info('Ruuvi Gateway starting...');
 
 noble.on('stateChange', (state) => {
@@ -27,23 +31,35 @@ noble.on('discover', (peripheral) => {
     if (data) {
       log.debug(`Got ${peripheral.id} ${JSON.stringify(data)}`);
 
-      const auth = {
-        username: process.env.DATA_INBOUND_API_USERNAME,
-        password: process.env.DATA_INBOUND_API_PASSWORD,
-      };
+      previousData[peripheral.id] = data;
 
-      axios.post(process.env.DATA_INBOUND_API_URL, {
-          id: peripheral.id,
-          data,
-        }, {
-          auth
-        })
-        .then(() => {
-          log.debug(`Successfully send data to the server for ID ${peripheral.id}.`);
-        })
-        .catch((e) => {
-          log.log(`Failed sending data to the serve ${e}.`);
-        });
+      buffer.push({
+        id: peripheral.id,
+        timestamp: new Date().getTime(),
+        data,
+      });
+
+      // Size buffer by number of detected devices and buffer size
+      const bufferLimit = Object.keys(previousData) * perDeviceBufferSize;
+
+      if (buffer.length > bufferLimit) {
+        const auth = {
+          username: process.env.DATA_INBOUND_API_USERNAME,
+          password: process.env.DATA_INBOUND_API_PASSWORD,
+        };
+
+        const dataToSend = buffer.splice(0, buffer.length);
+
+        axios.post(process.env.DATA_INBOUND_API_URL, dataToSend, {
+            auth
+          })
+          .then(() => {
+            log.debug(`Successfully send ${dataToSend.length} measurements to the server.`);
+          })
+          .catch((e) => {
+            log.log(`Failed sending data to the serve ${e}.`);
+          });
+      }
     }
   }
 });
